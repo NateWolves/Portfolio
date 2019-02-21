d2290e2f3be1e87494ccbe3f8d69ec6a9ce05ab1
unknown <wolverton.nathan@gmail.com>
2019-02-21 00:15:10 -0600

    Canvas physics have been ironed out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -210,6 +210,7 @@ canvas.addEventListener('mousedown', function(e){
 addEventListener("mousemove", function(event){
     mouse.x = event.clientX;
     mouse.y = event.clientY;
+    console.log(mouse.y)
 });
 
 // resizing our canvas on a screen change
@@ -237,62 +238,70 @@ function getDistance(x1, y1, x2, y2){
     return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
  
 }
-
-function rotate(velocity, angle){
+  
+//Gets distance between two balls surfaces
+  function surfaceDistance(ball1, ball2) {  
+    var center_dist = getDistance(ball1.x, ball1.y, ball2.x, ball2.y);
+    return center_dist - (ball1.radius + ball2.radius);
+  }
+// Rotates our circles to be on the same x-axis
+function rotate(velocity, angle) {
     const rotatedVelocities = {
-        x: velocity.x * Math.cos(angle) - 
-            velocity.y * Math.sin(angle),
-        y: velocity.x * Math.sin(angle) +
-            velocity.y * Math.cos(angle)
+        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
     };
+
     return rotatedVelocities;
-};
-
-
-function collision(ball, otherBall){
-    const xVelocityDiff = ball.velocity.x - otherBall.velocity.x;
-    const yVelocityDiff = ball.velocity.y - otherBall.velocity.y;
-
-    const xDist = otherBall.x - ball.x;
-    const yDist = otherBall.y - ball.y;
-
-    // Preventing any overlap between balls
-    if(xVelocityDiff * xDist + yVelocityDiff * yDist >= 0){
-
-        // Getting an angle between the two colliding balls
-        const angle = Math.atan2(otherBall.y - ball.y, otherBall.x - ball.x);
-
-        // Storing mass for easier access
-        const m1 = ball.mass;
-        const m2 = otherBall.mass;
-
-        // Velocity before equation
-        const u1 = rotate(ball.velocity, angle)
-        const u2 = rotate(otherBall.velocity, angle)
-        //  Velocity after collision
-        const v1 = {x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y};
-        const v2 = {x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y};
-        // Final velocity after rotating axis back to  original location
-        const vFinal1 = rotate(v1, -angle);
-        const vFinal2 = rotate(v2, -angle);
-        // Swapping ball velocities for a bounce effect
-        ball.velocity.x = vFinal1.x * 0.9;
-        ball.velocity.y = vFinal1.y * 0.9;
-
-        otherBall.velocity.x = vFinal2.x * 0.9;
-        otherBall.velocity.y = vFinal2.y * 0.9;
-    }
 }
 
-const gravity = 0.5;
+
+
+function resolveCollision(particle, otherParticle) {
+    const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+    const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+
+    const xDist = otherParticle.x - particle.x;
+    const yDist = otherParticle.y - particle.y;
+
+    // Prevent accidental overlap of particles
+    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+
+        // Grab angle between the two colliding particles
+        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+
+        // Store mass in var for better readability in collision equation
+        const m1 = particle.mass;
+        const m2 = otherParticle.mass;
+
+        // Velocity before equation
+        const u1 = rotate(particle.velocity, angle);
+        const u2 = rotate(otherParticle.velocity, angle);
+
+        // Velocity after 1d collision equation
+        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
+        const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+
+        // Final velocity after rotating axis back to original location
+        const vFinal1 = rotate(v1, -angle);
+        const vFinal2 = rotate(v2, -angle);
+
+        // Swap particle velocities for realistic bounce effect
+        particle.velocity.x = vFinal1.x * .8;
+        particle.velocity.y = vFinal1.y * .8;
+
+        otherParticle.velocity.x = vFinal2.x * .8;
+        otherParticle.velocity.y = vFinal2.y * .8;
+    }
+}
+const gravity = 0.1;
 // Creating our ball object to put on the canvas 
-function Ball(x, y, dx, dy,  radius){
+function Ball(x, y, dx, dy, radius){
     // passing our values to the object
     this.x = x;
     this.y = y;
     this.velocity = {
         x: dx,
-        y: dy,
+        y: dy
     };
     this.radius = radius;
     this.color = colors[Math.floor(Math.random() * colors.length)];
@@ -305,34 +314,59 @@ function Ball(x, y, dx, dy,  radius){
         for ( let i = 0; i < ballArray.length; i++){
             // never comparing a ball to itself
             if(this === ballArray[i]) continue;
-            if (getDistance(this.x, this.y, ballArray[i].x , ballArray[i].y) - this.radius * 2 < 0) {
-                console.log("Ball has collided.")
-                collision( this, ballArray[i]);
+
+            let sd = surfaceDistance(this, ballArray[i]);
+            let cd = getDistance(this.x, this.y, ballArray[i].x , ballArray[i].y)
+
+            if (sd < 0 ) { 
+                //get depth of overlap for x & y (based on ratio of sd/cd to x & y depth/diff),
+                var x_diff = this.x - ballArray[i].x;  // x difference between balls
+                var y_diff = this.y - ballArray[i].y;  // y difference between balls
+                var x_depth = x_diff * sd / cd;  // x depth of overlap
+                var y_depth = y_diff * sd / cd;  // y depth of overlap
+                //and move balls x & y values back by half of shared depth each (altering velocities)
+                this.x -= x_depth * 0.5;
+                this.y -= y_depth * 0.5;
+                ballArray[i].x += x_depth * 0.5;
+                ballArray[i].y += y_depth * 0.5;
+            };
+            if ( cd - (this.radius + ballArray[i].radius) < 0) {
+               
+                resolveCollision( this, ballArray[i]);
             }
         }
-
-        if (this.x - this.radius <= 0 || this.x + this.radius + this.velocity.x >= canvas.width) {
+        // Keeping the ball inside the left wall 
+        if (this.x - this.radius <= 0 ) {
             this.velocity.x = -this.velocity.x;
+            this.x = 0 + this.radius;
+        }
+        // Keeping the ball inside the right wall
+        if (this.x + this.radius + this.velocity.x >= canvas.width){
+            this.velocity.x = -this.velocity.x;
+            this.x = canvas.width - this.radius;
         }
 
         // This is applying the affect of gravity to our object when it collides with the bottom edge
-        if (this.y - this.radius <= 0 || this.y + this.radius + this.velocity.y >= canvas.height){
-            // applying friction
-            this.velocity.y = -this.velocity.y * 1;
-        } else {
+        if (this.y + this.radius + this.velocity.y >= canvas.height){
+            // keeping our balls above the bottom line
+            this.y = canvas.height - this.radius;
+            // applying friction 
+            this.velocity.y = -this.velocity.y * .8;
+        }
+         else {
+            
             this.velocity.y += gravity;
         }
         
     
         //  Adding reaction to the mouse 
-        if (getDistance(mouse.x, mouse.y, this.x , this.y) - this.radius * 2 < 0 ){
-            this.radians += 0.05;
-
+        if (getDistance(mouse.x, mouse.y, this.x , this.y) - this.radius * 4 < 0 ){
+            this.opacity = Math.max(.2, this.opacity)
             this.opacity += 0.06;
         } else if ( this.opacity > 0) {
-            this.opacity -= 0.02;
+            this.opacity -= 0.07;
 
-            this.opacity = Math.max(0, this.opacity);
+            this.opacity = Math.max(.2, this.opacity);
         }
 
         this.x += this.velocity.x; 
@@ -360,11 +394,11 @@ let ball;
 function init() {
     ballArray = [];
     for (let i = 0; i < 20; i++){
-        let radius = 50;
+        let radius = randomInteger (50, 50);
         let x = randomInteger(radius, canvas.width - radius);
         let y = randomInteger(radius, canvas.height- radius);
-        let dx = randomInteger(0, 0);
-        let dy = randomInteger(0, 0);
+        let dx = randomInteger(-1, 1);
+        let dy = randomInteger(-1, 1);
         if ( i !== 0){
             for ( let j = 0 ; j < ballArray.length; j++){
                 if (getDistance(x, y, ballArray[j].x , ballArray[j].y) - radius * 2 < 0) {
